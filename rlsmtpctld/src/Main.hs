c2d833bd55a437c54914f8c00e46087748a98ac2
Kilian Kwaschik <kwaschik@users.noreply.github.com>
2020-11-01 15:02:26 +0100

    www should own unix socket file

--- rlsmtpctld/src/Main.hs
+++ rlsmtpctld/src/Main.hs
@@ -10,6 +10,7 @@ import System.Directory (removeFile)
 import System.IO (hGetContents)
 import System.Posix.Daemonize (daemonize)
 import System.Posix.Files (setOwnerAndGroup)
+import System.Posix.User (UserEntry(..), getUserEntryForName)
 import System.Posix.Signals
 import System.Process (createProcess, proc, std_out, StdStream(CreatePipe))
 
@@ -24,14 +25,15 @@ main = daemonize $ do
         sock <- socket AF_UNIX Stream defaultProtocol
         bind sock $ SockAddrUnix sockfile
         installHandler sigTERM (Catch $ cleanUp sock sockfile) Nothing
-        setOwnerAndGroup sockfile 1000 1000
+        www <- getUserEntryForName "www"
+        setOwnerAndGroup sockfile (userID www) (userGroupID www)
         listen sock 5
         (forever $ do
                 (csock, peer) <- accept sock
                 msg <- recv csock 1024
                 answer <- if "smtpctl" `isPrefixOf` msg
                           then fmap pack smtpctl
-                          else return "wrong command"
+                          else return "unknown command\n"
                 sendAll csock answer
                 gracefulClose csock 1000) `finally` 
                 (cleanUp sock sockfile)
