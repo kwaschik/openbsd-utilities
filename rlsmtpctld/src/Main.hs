652a1ec80541270aff2cad1319a33d72453cd82b
user <user@opi.v.cablecom.net>
2021-04-26 18:31:27 +0200

    pledged and unveiled rlsmtpctld

--- rlsmtpctld/src/Main.hs
+++ rlsmtpctld/src/Main.hs
@@ -1,9 +1,13 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
 module Main where
 
 import Control.Monad (forever)
 import Control.Exception (finally)
 import Data.ByteString.Char8 (append, isPrefixOf, pack)
+import Foreign.C.String
+import Foreign.C.Types
+import Foreign.Ptr
 import Network.Socket
 import Network.Socket.ByteString (recv, sendAll)
 import System.Directory (removeFile)
@@ -14,6 +18,11 @@ import System.Posix.User (UserEntry(..), getUserEntryForName)
 import System.Posix.Signals
 import System.Process (createProcess, proc, std_out, StdStream(CreatePipe))
 
+foreign import ccall "unveil"
+        unveil :: CString -> CString -> IO CInt
+foreign import ccall "pledge"
+        pledge :: CString -> CString -> IO CInt
+
 
 sockfile = "/var/www/rlsmtpctl.sock"
 
@@ -22,12 +31,16 @@ cleanUp s f = close s >> removeFile f
 
 main :: IO ()
 main = daemonize $ do
+        call unveil sockfile "rwc"
+        call unveil "/usr/sbin/smtpctl" "x"
+        call pledge "stdio unix rpath cpath getpw chown proc exec" "NULL"
         sock <- socket AF_UNIX Stream defaultProtocol
         bind sock $ SockAddrUnix sockfile
         installHandler sigTERM (Catch $ cleanUp sock sockfile) Nothing
         www <- getUserEntryForName "www"
         setOwnerAndGroup sockfile (userID www) (userGroupID www)
         listen sock 5
+        call pledge "stdio unix cpath proc exec" "NULL"
         (forever $ do
                 (csock, peer) <- accept sock
                 msg <- recv csock 1024
@@ -37,7 +50,11 @@ main = daemonize $ do
                 sendAll csock answer
                 gracefulClose csock 1000) `finally` 
                 (cleanUp sock sockfile)
-        where smtpctl = do
+        where call syscall str1 "NULL" =
+                withCString str1 $ \p1 -> syscall p1 nullPtr
+              call syscall str1 str2 = 
+                withCString str1 $ \p1 -> withCString str2 $ \p2 -> syscall p1 p2
+              smtpctl = do
                 (_, Just hout, _, _) <-
                         createProcess (proc "smtpctl" ["update", "table", "vpasswd"]) { 
                                 std_out = CreatePipe
